8f96d6c187480954f03de68311155c66cc752477
Anisha A <anishaarul605@gmail.com>
2024-11-02 15:29:17 +0530

    Add files via upload

---
+++ Employee Management System Using Mysql.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO employees (first_name,last_name, department, salary, hire_date) Values ('john','doe','Engineering', 90.0 ,'2022-02-15'), ('Ani','Sai','Marketing', 900.0 ,'2023-03-15'), ('Swe','arul','IAS', 90000.0 ,'2024-02-15'), ('prithi','vinoth','Social_enthusiasist', 9000000.0 ,'2026-03-15');
+Select * from employees;
